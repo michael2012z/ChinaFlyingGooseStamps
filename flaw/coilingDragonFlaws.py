3e0c1755cf65bee3e3aeed348cf80f33a3c11414
Michael Z <michael2012zhao@yahoo.com>
2015-05-24 12:58:50 +0800

    generate icons

--- flaw/coilingDragonFlaws.py
+++ flaw/coilingDragonFlaws.py
@@ -8,6 +8,14 @@ r = 12
 l = 10
 # dot size
 d = 4
+# icon dot size
+dotSizeS = 10
+
+def markFlawPointS(imageDraw, x, y):
+    dotSize = dotSizeS
+    x = int(x)
+    y = int(y)
+    imageDraw.ellipse([x-dotSize, y-dotSize, x+dotSize, y+dotSize], 'blue')
 
 def _markFlawPoint(imageDraw, x, y, lineSize, dotSize):
     x = int(x)
@@ -60,9 +68,11 @@ def normalize(x1, y1, x2=0, y2=0):
 
 def generateModels(name):
     im = None
+    imS = None
     draw = None
     w, h = 0, 0
     draw = None
+    drawS = None
     stampID = 0
     serial = 0
 
@@ -75,6 +85,11 @@ def generateModels(name):
             # release existing things
             if im <> None:
                 im.save('model/' + name + '/' + 'model_' + name + '_' + str(stampID/10) + str(stampID%10) + '.png', 'png')
+                tmpW, tmpH = imS.size
+                tmpW /= 2
+                tmpH /= 2
+                imS = imS.resize((tmpW, tmpH), Image.ANTIALIAS)
+                imS.save('model/' + name + '/' + 'model_' + name + '_' + str(stampID/10) + str(stampID%10) + '_s.png', 'png')
                 im = None
                 draw = None
             # setup new ctxt
@@ -83,7 +98,9 @@ def generateModels(name):
                 return
             stampID = int(stampID)
             im = Image.open('model/model_' + name + '.png')
+            imS = Image.open('model/model_' + name + '.png')
             draw = ImageDraw.Draw(im)
+            drawS = ImageDraw.Draw(imS)
             serial = 0
             print "stamp: " + str(stampID)
         if line.find("-") >= 0:
@@ -93,6 +110,7 @@ def generateModels(name):
             x2, y2 = line.split("-")[1].split(",")[0], line.split("-")[1].split(",")[1]
             x1, y1, x2, y2 = normalize(x1, y1, x2, y2)
             markFlawLine(draw, x1, y1, x2, y2, serial)
+            markFlawPointS(drawS, 1.0*(x1+x2)/2, 1.0*(y1+y2)/2)
             print "line: " + str([x1, y1, x2, y2])
         elif line.find(",") >= 0:
             # this is a dot flaw
@@ -100,6 +118,7 @@ def generateModels(name):
             x, y = line.split(",")[0], line.split(",")[1]
             x, y, XXX, YYY = normalize(x, y)
             markFlawPoint(draw, x, y, serial)
+            markFlawPointS(drawS, x, y)
             print "dot: " + str([x, y])
             
     f.close()
