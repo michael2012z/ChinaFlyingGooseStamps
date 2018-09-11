81829c52df97ffa71ed1ffa50594921d693af378
Michael Z <michael2012zhao@hotmail.com>
2018-09-11 12:28:27 +0100

    library page

---
+++ tools/generate_library_pages.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import os, time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def generate_lib_pages (name, nameC, category, categoryC):
+    faceFiles = []
+    categoryDir = '../' + name + '/library/' + category
+    categoryUrlDir = name + '/library/' + category
+    srcDir = categoryDir + '/src/'
+    srcUrlDir = categoryUrlDir + '/src/'
+    snpDir = categoryDir + '/m_size/'
+    snpUrlDir = categoryUrlDir + '/m_size/'
+    imageFiles = os.listdir(srcDir)
+    imageFiles.sort()
+    for imageFile in imageFiles[::-1]:
+	if os.path.isfile( srcDir + imageFile) and os.path.splitext(imageFile)[1] == ".jpg" and os.path.splitext(imageFile)[0].find("A") > 0 and os.path.splitext(imageFile)[0].find("A") == (len(os.path.splitext(imageFile)[0])-1):
+	    faceFiles.append([srcUrlDir + imageFile, snpUrlDir+ os.path.splitext(imageFile)[0] + ".jpg"]) 
+
+    pageContent = ''
+    count = 0
+    for faceFile in faceFiles:
+	if count % 4 == 0:
+	    pageContent += "<div>"
+	pageContent += "<a target=\"_blank\" href=\"" + baseUrl
+	pageContent += faceFile[0]
+	pageContent += "\">"
+	pageContent += "<img width=\"280\" src=\"" + baseUrl
+	pageContent += faceFile[1]
+	pageContent += "\"></a>"
+	if count % 4 == 3:
+	    pageContent += "</div>\r\n"
+	count += 1
+    if count % 4 <> 0:
+	pageContent += "</div>\r\n"
+
+    templateFile = open('page_templates/library_page_template.html')
+    template = templateFile.read()
+    templateFile.close()
+    
+    template = template.replace('[REPLACE_NAME_TEXT]', nameC)
+    template = template.replace('[REPLACE_STAMP_LIST]', pageContent)
+
+    # write page
+    f = open ('../' + name + '/library/' + category + '.html', 'w')
+    f.write(template)
+    f.close()
+
+    
+if __name__ == '__main__':
+    stamps = [
+        {'name':'1d', 'nameC':'壹圆'},
+        {'name':'2d', 'nameC':'贰圆'},
+        {'name':'5d', 'nameC':'伍圆'},
+    ]
+
+    for stamp in stamps:
+        generate_lib_pages (stamp['name'], stamp['nameC'])
+
