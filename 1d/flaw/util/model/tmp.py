1ff65025543130fea34c8618b3bde89891eabd16
Michael Z <michael2012zhao@hotmail.com>
2019-05-25 07:07:57 -0400

    restruct

--- 1d/flaw/util/model/tmp.py
+++ 1d/flaw/util/model/tmp.py
@@ -1,12 +1,12 @@
 import os
 
 for i in range(1, 49):
-    fs = 'generated/model_1d_' + str(i) + '.png'
+    fs = 'generated/model_1d_%02d.png' %(i)
     ft = '../../%02d/model.png' %(i)
     os.rename(fs, ft)
     
 for i in range(1, 49):
-    fs = 'generated/model_1d_' + str(i) + '_s.png'
+    fs = 'generated/model_1d_%02d_s.png' %(i)
     ft = '../../%02d/model_s.png' %(i)
     os.rename(fs, ft)
     
