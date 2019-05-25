582c61b67a77aba51bc258be452492b9654f4127
Michael Z <michael2012zhao@hotmail.com>
2019-05-25 07:04:31 -0400

    tmp

--- 1d/flaw/util/model/tmp.py
+++ 1d/flaw/util/model/tmp.py
@@ -1,7 +1,12 @@
 import os
 
 for i in range(1, 49):
-    fs = 'model (' + str(i) + ').PNG'
-    ft = '../../../../%02d/model.png' %(i)
+    fs = 'generated/model_1d_' + str(i) + '.png'
+    ft = '../../%02d/model.png' %(i)
+    os.rename(fs, ft)
+    
+for i in range(1, 49):
+    fs = 'generated/model_1d_' + str(i) + '_s.png'
+    ft = '../../%02d/model_s.png' %(i)
     os.rename(fs, ft)
     
